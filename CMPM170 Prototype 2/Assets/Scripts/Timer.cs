1ed98369dcc151454d39560475ee1b9b3858d106
dexhoang <dhhoang@ucsc.eedu>
2024-11-03 23:09:25 -0800

    Prototype 1 pt. 1

--- CMPM170 Prototype 2/Assets/Scripts/Timer.cs
+++ CMPM170 Prototype 2/Assets/Scripts/Timer.cs
@@ -12,6 +12,29 @@ public class Timer : MonoBehaviour
     public float minTime = 5f;
     public float maxTime = 10f;
 
+    private float[] rotationOptions = { 0f, 90f, 180f, 270f };
+    private bool isRotating = false;
+    private float start = 0f;
+    private float end = 0f;
+    IEnumerator RotateOverTime(float angle, float duration)
+    {
+        isRotating = true;
+
+        Quaternion startRotation = transform.rotation;
+        Quaternion endRotation = startRotation * Quaternion.Euler(angle, 0, 0);
+        float elapsedTime = 0f;
+
+        while (elapsedTime < duration)
+        {
+            transform.rotation = Quaternion.Slerp(startRotation, endRotation, elapsedTime / duration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.rotation = endRotation;
+        isRotating = false;
+    }
+
     private void Start()
     {
         TimerOn = true;
@@ -37,7 +60,15 @@ public class Timer : MonoBehaviour
                 TimeLeft = 0;
                 TimerOn = false;
                 Debug.Log("Timer is Up");
+                if (!isRotating)
+                {
 
+                    float randomAngle = rotationOptions[Random.Range(0, rotationOptions.Length)];
+
+                    float rotationDuration = Mathf.Abs(transform.eulerAngles.x - randomAngle / 90f) * 2f;
+
+                    StartCoroutine(RotateOverTime(randomAngle, 2f));
+                }
                 RestartTimer();
             }
         }
