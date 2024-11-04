96d516493b369331392e5c8d17e8460a6602eb35
Alan Lu <alu36@ucsc.edu>
2024-11-03 22:35:19 -0800

    rotation

--- CMPM170 Prototype 2/Assets/Scripts/Map.cs
+++ CMPM170 Prototype 2/Assets/Scripts/Map.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class Map : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float[] rotationOptions = {0f, 90f, 180f, 270f};
+    private bool isRotating = false;
+    private float start = 0f;
+    private float end = 0f;
 
-    // Update is called once per frame
+    IEnumerator RotateOverTime(float angle, float duration)
+    {
+        isRotating = true;
+
+        Quaternion startRotation = transform.rotation;
+        Quaternion endRotation = startRotation * Quaternion.Euler(angle, 0, 0);
+        float elapsedTime = 0f;
+
+        while (elapsedTime < duration)
+        {
+            transform.rotation = Quaternion.Slerp(startRotation, endRotation, elapsedTime / duration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.rotation = endRotation;
+        isRotating = false;
+    }
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.U) && !isRotating)
+        {
+
+            float randomAngle = rotationOptions[Random.Range(0, rotationOptions.Length)];
+
+            float rotationDuration = Mathf.Abs(transform.eulerAngles.x - randomAngle / 90f) * 2f;
+
+            StartCoroutine(RotateOverTime(randomAngle, 2f));
+        }
     }
 }
